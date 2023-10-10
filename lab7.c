f8af11bc3204dfd9b6cf9c1f7f5a3c9a9716dfce
eunkyuleee <lek7146@gmail.com>
2023-10-10 11:28:12 +0000

    lab 7

--- lab7.c
+++ lab7.c
@@ -4,10 +4,9 @@
 
 int main()
 {
-	char *cwd;
 	char buf[256];
 
-	cwd = getcwd(buf, 256);
-	setenv("PATH", cwd, 1);
+	getcwd(buf, 256);
+	setenv("PATH", buf, 1);
 	printf("current PATH env: %s\n", getenv("PATH"));
 }
